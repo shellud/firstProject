93a0cff5f7383a537b94cf6cd9512449132813dd
shellud <shellud@gmail.com>
2023-01-14 14:22:09 +0200

    Homework 2

--- src/com/ua/robot/homework2/Main.java
+++ src/com/ua/robot/homework2/Main.java
@@ -2,6 +2,6 @@ package com.ua.robot.homework2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
     }
 }
