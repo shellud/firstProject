50cd092fa94565cb3796d99b31335aa644ae2911
shellud <shellud@gmail.com>
2023-01-17 16:32:28 +0200

    Homework 3

---
+++ src/com/ua/robot/homework3/Main.java
@@ -0,0 +1,12 @@
+package com.ua.robot.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 5;
+        System.out.println("Результат додавання:" + (a+b));
+        System.out.println("Результат віднімання:" + (a-b));
+        System.out.println("Результат ділення:" + (a/b));
+        System.out.println("Результат множення:" + (a*b));
+    }
+}
