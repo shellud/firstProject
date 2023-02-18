30206a38e0be5781901a38b7c3df2a4ebcdb46d7
shellud <shellud@gmail.com>
2023-02-18 13:21:34 +0200

    Homework 12

---
+++ src/com/ua/robot/homework12/Child.java
@@ -0,0 +1,15 @@
+package com.ua.robot.homework12;
+
+public class Child extends Parent{
+    static {
+        System.out.println("Child 2");
+    }
+
+    {
+        System.out.println("Child 5");
+    }
+
+    {
+        System.out.println("Child 6");
+    }
+}
---
+++ src/com/ua/robot/homework12/Main.java
@@ -0,0 +1,13 @@
+package com.ua.robot.homework12;
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        System.out.println();
+        Child child = new Child();
+        System.out.println();
+        Child child2 = new Child();
+        System.out.println();
+        Parent parent2 = new Parent();
+    }
+}
